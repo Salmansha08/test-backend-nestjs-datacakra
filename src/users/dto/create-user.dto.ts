b3baa5c8be6a7ed15169c2dc7b5db00a0989dc95
Salmansha08 <salmanwiharja8@gmail.com>
2024-10-26 13:24:09 +0700

    fix: Api Docs for create user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,22 +2,22 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty({ example: 'John Doe' })
+  @ApiProperty({ example: 'Salman Wiharja' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
-  @ApiProperty({ example: 'email@example.com' })
+  @ApiProperty({ example: 'salman@gmail.com' })
   @IsEmail()
   email: string;
 
-  @ApiProperty({ example: 'password123456' })
+  @ApiProperty({ example: 'salman123' })
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters' })
   @IsNotEmpty()
   password: string;
 
-  @ApiProperty({ example: 'asidas9j2j23i209' })
+  @ApiProperty({ example: 'ROLE_ID' })
   @IsString()
   @IsNotEmpty()
   roleId: string;
