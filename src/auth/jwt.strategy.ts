2f9ec7fca6455beda8179ae1ce8bdc1d882af5a8
Salmansha08 <salmanwiharja8@gmail.com>
2024-10-31 14:22:46 +0700

    fix: remove no used import service

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,17 +1,12 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { RoleType } from '@prisma/client';
-import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private readonly userService: UsersService,
-    private readonly authService: AuthService,
-  ) {
+  constructor(private readonly authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
